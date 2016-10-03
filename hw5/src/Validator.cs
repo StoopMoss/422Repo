e035de686e8d2de18a619b8e8138eab64a8a7aa2
cj-muse <connermattingly@yahoo.com>
2016-10-02 20:07:19 -0700

    created a validator class to encapsulate variables that need to be thread specific

--- hw5/src/Validator.cs
+++ hw5/src/Validator.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Net.Sockets;
-using System.Net;
+using System;
 using System.Text;
-using System.IO;
 
 namespace CS422
 {
-	public class WebServer
-	{				
+	public class Validator
+	{
+		private byte[] _buffer;
 		private string _URL;
 
 		//Private members used for validation
@@ -20,16 +18,38 @@ namespace CS422
 		private bool _fullHeaders;
 		private bool _fullRequest;
 
-		private 
+		////////////////
+		// Properties
+		////////////////
+		public string URL
+		{
+			get{return _URL;}
+			set{_URL = value;}
+		}
 
-		public WebServer ()
-		{}
+		public byte[] Buffer
+		{
+			get{return _buffer;}
+			set{_buffer = value;}
+		}
 
-		public static bool Start(int port, int numberOfThreads)
-		{	
-			bool active = true;
+		/////////////
+		//Methods
+		////////////
+		public Validator()
+		{			
+			SetMembers();			
+		}
 
-			// Set validation flags
+		public Validator(byte[] buffer)
+		{
+			Buffer = buffer;
+			SetMembers();			
+		}
+
+		public void SetMembers()
+		{
+			_URL = "";
 			_validRequestLine = false;
 			_validRequestHeaders = false;
 
@@ -38,99 +58,9 @@ namespace CS422
 			_fullURI = false;
 			_fullHeaders = false;
 			_fullRequest = false;
-
-			if (numberOfThreads <= 0)
-			{
-				numberOfThreads = 64;
-			}
-			
-			// Set TCP 
-			TcpClient client = new TcpClient();
-			TcpListener listener = new TcpListener(IPAddress.Any, port);
-			listener.Start();
-			Console.WriteLine ("started TCPListener");
-
-
-			/*
-			• Accept	new	TCP	socket	connection
-			• Get a	thread	from	the	thread	pool	and	pass	it	the	TCP	socket
-			• Repeat
-			*/
-
-//			while (active) 
-//			{
-//				Console.WriteLine("Hi from ");
-//				// blocking call to accept client
-//				client = listener.AcceptTcpClient();
-//				NetworkStream networkStream = client.GetStream();
-//
-//				// read and validate what was read
-//				active = ReadFromNetworkStream(networkStream);
-//				Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-//				Console.WriteLine("Just read from ");
-//
-//				if (active)
-//				{
-//					Console.WriteLine("about to Write");
-//					WriteResponseToClient (networkStream, responseTemplate);
-//				}
-//				else 
-//				{
-//					// read failed, close connection
-//					networkStream.Dispose();
-//					client.Close();
-//					return active;
-//				}
-//				networkStream.Dispose();
-//			}
-
-			client.Close();
-			return true;			
 		}
 
-		// 
-		// Request methods
-		//
-		// Reads from network stream and validates as it goes
-		public bool ReadFromNetworkStream(Stream networkStream)
-		{
-			byte[] buffer = new byte[4096];
-			int bytesRead = 0, totalBytesRead = 0;
-			bool validRequest = false;
-
-			//Console.WriteLine("in Read");
-			if (networkStream.CanRead) 
-			{
-				do {
-					// read from stream until buffer is full or Read() returns 0
-					bytesRead = networkStream.Read (buffer, totalBytesRead, buffer.Length - totalBytesRead);
-					if (bytesRead == 0)
-					{
-						break;
-					}
-					Console.WriteLine("Read " + bytesRead.ToString() + "bytes");
-					validRequest = ValidateRequest(buffer);
-
-					// If valid and full no more reading is required
-					// If not full then request has not been read fully
-					if ( _fullRequest && validRequest)
-					{
-						// stop reading and send back a response
-						return true;
-					}
-
-
-				} while (validRequest);
-			}
-			return false;
-		}
-
-		private WebRequest BuildRequest(TcpClient client)
-		{
-			
-		}
-
-		public static bool ValidateRequest(byte[] buffer)
+		public bool ValidateRequest(byte[] buffer)
 		{	
 			if (!_fullMethod)
 			{
@@ -152,8 +82,7 @@ namespace CS422
 				Console.WriteLine("Validate: Check Version");
 				if(!ValidateRequestVersion(buffer) )
 				{ return false; }
-
-				if(_fullVersion) 
+					if(_fullVersion) 
 				{
 					// Entire first line is full and valid
 					_validRequestLine = true;
@@ -168,32 +97,27 @@ namespace CS422
 					return false; 
 				}											
 			}
-
-			Console.WriteLine("_fullMethod "+ 
+				Console.WriteLine("_fullMethod "+ 
 				_fullMethod.ToString()+"_fullURI "+ 
 				_fullURI.ToString()+"_fullVersion"+
 				_fullVersion.ToString()+"  _fullHeaders)"+
 				_fullHeaders.ToString());
-
-			//TODO: do i need full headers?
+				//TODO: do i need full headers?
 			if (_fullMethod && _fullURI && _fullVersion && _fullHeaders)
 			{
 				_fullRequest = true;
 			}
-
-			return true;
+				return true;
 		}
 
-
 		//accounts for space  "GET "
-		public static bool ValidateRequestMethod (byte[] buffer)
+		public bool ValidateRequestMethod (byte[] buffer)
 		{			
 			string requestSoFar = Encoding.ASCII.GetString(buffer);
 			string expectedString = "GET ";
 			int bytesToCheck = 4;
 
 			Console.WriteLine("Validating request Method");
-
 			if (requestSoFar.Length == 0)
 			{
 				//error...
@@ -207,7 +131,6 @@ namespace CS422
 			{				
 				bytesToCheck = requestSoFar.Length;
 			}
-
 			for (int i = 0; i < bytesToCheck; i++)
 			{
 				if (requestSoFar[i] != expectedString[i])
@@ -218,13 +141,12 @@ namespace CS422
 			return true;				
 		}
 
-		public static bool ValidateRequestURI(byte[] buffer)
+		public bool ValidateRequestURI(byte[] buffer)
 		{
 			// account for space "URI/ "
 			// assume everything before is correct "GET "
 			// Set _URL to be used later
 			Console.WriteLine("Validating request URI");
-
 			string requestSoFar = Encoding.ASCII.GetString(buffer);
 			int i = 0;
 			int whitespaces = 0;
@@ -257,7 +179,7 @@ namespace CS422
 		}
 
 		// Must account for newline
-		public static bool ValidateRequestVersion (byte[] buffer)
+		public bool ValidateRequestVersion (byte[] buffer)
 		{			
 			string requestSoFar = Encoding.ASCII.GetString(buffer);
 			string versionSubstring;
@@ -283,17 +205,14 @@ namespace CS422
 				}
 				i++;
 			}
-
 			// TODO: make more robust i.e. garbage after version, how are line ends compared?						
 			return true;
 		}
 
-		public static bool ValidateRequestHeaders(byte[] buffer)
+		public bool ValidateRequestHeaders(byte[] buffer)
 		{
 			String bufferAsString = Encoding.ASCII.GetString(buffer);
 			bufferAsString = bufferAsString.Substring(0, bufferAsString.IndexOf("\r\n\r\n"));
-
-
 			string[] headers = bufferAsString.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
 
 			for (int i = 1; i < headers.Length; i++)
@@ -311,7 +230,8 @@ namespace CS422
 					{
 						Console.WriteLine("This line contains a colon AND a first-position letter.");
 					}
-					else{
+					else
+					{
 						Console.WriteLine("DARN it's returning false!!!!");
 						return false;
 					}
@@ -321,7 +241,7 @@ namespace CS422
 			return true;
 		}
 
-		public static int GetStartOfHeaderBufferIndex(byte[] buffer)
+		public int GetStartOfHeaderBufferIndex(byte[] buffer)
 		{
 			string requestReadSoFar = Encoding.ASCII.GetString(buffer);
 			int i = 0;
@@ -342,28 +262,5 @@ namespace CS422
 			}
 			return 0;
 		}
-
-		// 
-		// Response methods
-		//
-		public static bool WriteResponseToClient(NetworkStream networkStream, string responseTemplate)
-		{
-			int studentId = 11282717;
-
-			// fill 
-			string response = string.Format(responseTemplate, studentId, DateTime.Now, _URL);
-			byte[] buffer = Encoding.ASCII.GetBytes(response);
-
-			//Write to stream
-			if (networkStream.CanWrite)
-			{
-				networkStream.Write(buffer, 0, buffer.Length);
-				return true;
-			}
-
-			return false;
-		}
-
 	}
 }
-
---
+++ hw5/src/WebServer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace CS422
+{
+	public class WebServer
+	{	
+		//private readonly byte validatorLock;
+
+		public WebServer ()
+		{}
+
+		public static bool Start(int port, int numberOfThreads)
+		{	
+			bool active = true;
+						
+			if (numberOfThreads <= 0)
+			{
+				numberOfThreads = 64;
+			}
+			
+			// Set TCP 
+			TcpListener listener = new TcpListener(IPAddress.Any, port);
+			listener.Start();
+			Console.WriteLine ("started TCPListener");
+
+			while (active) 
+			{			
+				//
+				/*
+				• Accept	new	TCP	socket	connection
+				• Get a	thread	from	the	thread	pool	and	pass	it	the	TCP	socket
+				• Repeat
+				*/
+				Validator validator = new Validator();
+
+				TcpClient client = new TcpClient();
+				client = listener.AcceptTcpClient();
+				
+				NetworkStream networkStream = client.GetStream();
+
+				// read and validate what was read
+				active = ReadFromNetworkStream(networkStream);
+				
+				if (active)
+				{
+					Console.WriteLine("about to Write");
+					WriteResponseToClient (networkStream, responseTemplate, Validator);
+				}
+				else 
+				{
+					// read failed, close connection
+					networkStream.Dispose();
+					client.Close();
+					return active;
+				}
+				networkStream.Dispose();
+			}
+
+			client.Close();
+			return true;			
+		}
+
+		// 
+		// Request methods
+		//
+		// Reads from network stream and validates as it goes
+		public static bool ReadFromNetworkStream(Stream networkStream)
+		{
+			byte[] buffer = new byte[4096];
+			int bytesRead = 0, totalBytesRead = 0;
+			
+			// //Console.WriteLine("in Read");
+			// if (networkStream.CanRead) 
+			// {
+			// 	do {
+			// 		// read from stream until buffer is full or Read() returns 0
+			// 		bytesRead = networkStream.Read (buffer, totalBytesRead, buffer.Length - totalBytesRead);
+			// 		if (bytesRead == 0)
+			// 		{
+			// 			break;
+			// 		}
+			// 		Console.WriteLine("Read " + bytesRead.ToString() + "bytes");
+			//		validator.ValidateRequest(buffer);
+			// 		validRequest = ValidateRequest(buffer);
+
+			// 		// If valid and full no more reading is required
+			// 		// If not full then request has not been read fully
+			// 		if ( _fullRequest && validRequest)
+			// 		{
+			// 			// stop reading and send back a response
+			// 			return true;
+			// 		}
+
+
+			// 	} while (validRequest);
+			// }
+			return false;
+		}
+
+		private static WebRequest BuildRequest(TcpClient client)
+		{
+			return new WebRequest();
+		}		
+
+		// 
+		// Response methods
+		//
+		public static bool WriteResponseToClient(NetworkStream networkStream, string responseTemplate, Validator validator)
+		{
+			int studentId = 11282717;
+
+			// fill 
+			string response = string.Format(responseTemplate, studentId, DateTime.Now, Validator.URL);
+			byte[] buffer = Encoding.ASCII.GetBytes(response);
+
+			//Write to stream
+			if (networkStream.CanWrite)
+			{
+				networkStream.Write(buffer, 0, buffer.Length);
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+}
+
