556e365c29c47c378e854ab7f2fc06c22fc84114
cj-muse <connermattingly@yahoo.com>
2016-11-02 15:45:54 -0700

    currently trying to finish ToString on BigNum

--- hw10/BigNumLib/BigNum.cs
+++ hw10/BigNumLib/BigNum.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Numerics;
 
 namespace CS422
 {
@@ -7,12 +9,24 @@ namespace CS422
     // for double precision the significand can be 53 bits
     // This is about 16 decimal digits
     // the max number that 53 bits could store is (9,007,199,254,740,992)
-
     //TODO: make sure to restrict this value to a ceiling of (9,007,199,254,740,992)
-    private long _significand;
+    private BigInteger _mantissa;
+    //private long _mantissa;
 
     private int _exponent;
 
+    private bool _sign;
+
+    private bool _undefined;
+
+    //Properties for testing
+    public BigInteger Mantissa { get { return _mantissa; } }
+
+    public int Exponent { get { return _exponent; } }
+
+    public bool Sign { get { return _sign; } }
+
+    // Constructors
     public BigNum ()
     {
     }
@@ -24,6 +38,7 @@ namespace CS422
         //TODO: // the string is a valid number,
         // so initialize the BigNum to an exactly equal value. 
         // There must not be any rounding or truncation...
+        Initialize (number);
       }
       else
       {
@@ -33,6 +48,163 @@ namespace CS422
 
     public BigNum (double value, bool useDoubleToString)
     {
+      //Case 1
+      if (double.IsNaN (value) || double.IsInfinity (value))
+      {
+        // then the BigNum is initialized to an undefined value, 
+        // regardless of the value of the useDoubleToString parameter. 
+        _undefined = true;
+      }
+//      else if (useDoubleToString)
+//      {
+//        if ()
+//        Initialize (value);
+//      }
+
+      //Case 2
+
+
+      //Case
+    }
+
+
+    //////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+    /// Methods
+    /////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+    //
+    public override string ToString ()
+    {
+      StringBuilder builder = new StringBuilder ();
+
+      // FIrst check for sign
+      if (_sign)
+      {
+        builder.Append ("-");
+      }
+
+      //get the location of the decimal from the exponent
+      int index = _exponent * -1;
+      Console.WriteLine ("in");
+
+      if (index == 0) // no decimal
+      {
+        builder.Append (_mantissa.ToString ());
+        return builder.ToString ();
+      }
+      else if()// Move decimal left if exponent is  negative
+      {}
+      else if ()// Move decimal right if positive
+      {
+        
+      }
+
+      // append all digits before decimal
+      builder.Append (_mantissa.ToString ().Substring (0, index));
+
+      // Append decimal
+      builder.Append (".");
+
+      // append all digits after decimal
+      builder.Append (_mantissa.ToString ()
+        .Substring (index, _mantissa.ToString ().Length - index));
+
+      Console.WriteLine ("index: " + index);
+      Console.WriteLine ("_exponent: " + _exponent);
+      Console.WriteLine ("mantissa: " + _mantissa);
+      Console.WriteLine ("sign: " + _sign);
+
+      return builder.ToString ();
+    }
+
+
+    /////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+    // Utility Methods 
+    /////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+
+
+    // This is used by the constructor to initialize the members of the class 
+    public void Initialize (string number)
+    {
+      // See if there is a sign
+      _sign = (number.Contains ("-")) ? true : false;
+
+      // See if there is a decimal point and where it is
+      string str = number; 
+      int decimalIndex = number.IndexOf (".");
+      if (decimalIndex >= 0)
+      {
+        // We need to take care of trailing zeros (Leading zeros 
+        // seem to be handled by the parse of the string to BigInteger)
+        //Console.WriteLine ("string before removal: " + str);
+        str = RemoveTrailingZeros (number);
+        //Console.WriteLine ("string after removal: " + str);
+        
+        //then there is a decimal point so create exponent
+        _exponent = decimalIndex - (str.Length - 1);
+      }
+
+      // Now create the mantissa
+      char[] delimiters = { '.', '-' };
+      string[] parsedNumber = str.Split (delimiters);
+      StringBuilder b = new StringBuilder ();
+      foreach (string s in parsedNumber)
+      {
+        b.Append (s);
+      }
+
+      //We now have the mantisa in b
+      string r = b.ToString ();
+
+      _mantissa = new BigInteger ();
+      _mantissa = BigInteger.Parse (r);
+    }
+
+    public string RemoveTrailingZeros (string str)
+    {
+      StringBuilder builder = new StringBuilder ();
+      StringBuilder final = new StringBuilder ();
+
+      // first see if there is a decimal otherwise no operation is needed
+      if (!str.Contains ("."))
+      {
+        return str;
+      }
+
+      //store first and second parts of the string (second includes the decimal)
+      int index = str.IndexOf (".");
+      final.Append (str.Substring (0, index));
+      builder.Append (str.Substring (index, str.Length - index));
+      //Console.WriteLine ("length: " + str.Length.ToString ());
+      //Console.WriteLine ("first part of string " + final.ToString ());
+      //Console.WriteLine ("second part of string " + builder.ToString ());
+
+      //Iterate from the end of the string until a non zero digit, or a decimal 
+      // is encountered
+      int i = builder.Length - 1;
+      string partial = builder.ToString ();
+      while (partial [i] == '0')
+      {        
+        //Console.WriteLine ("Partial[i]: " + partial [i]);
+        i--;
+      }
+
+      //See if the last element compared is a decimal
+      if (partial [i] == '.')
+      {
+        // then everything after the decimal was a zero
+        // dont append anything
+      }
+      else
+      {
+        string s = builder.ToString ().Substring (0, i + 1);
+        final.Append (s);
+      }
+
+      return final.ToString ();
     }
 
     public static bool IsValidNumberString (string number)
--- hw10/BigNumTests/Test.cs
+++ hw10/BigNumTests/Test.cs
@@ -77,6 +77,237 @@ namespace BigNumTests
       }
     }
 
+
+    ////////////////////////////////////////////////////////////
+    /// The following tests validate the initialization function
+    /// of the BigNum class
+    [Test]
+    public void InitializeWithPlainValue ()
+    {
+      BigNum num = new BigNum ();
+      num.Initialize ("100");
+      Console.WriteLine (num.Mantissa.ToString ());
+      Assert.IsTrue (num.Mantissa.Equals (100));
+      Assert.AreEqual (0, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+    }
+
+    [Test]
+    public void InitializeWithNegativeValue ()
+    {
+      BigNum num = new BigNum ();
+      num.Initialize ("-100");
+      Assert.IsTrue (num.Mantissa.Equals (100));
+      Assert.AreEqual (0, num.Exponent);
+      Assert.AreEqual (true, num.Sign);
+    }
+
+    [Test]
+    public void InitializeWithDecimalValue ()
+    {
+      BigNum num = new BigNum ();
+      num.Initialize ("100.111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-3, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+
+      num.Initialize ("10011.1");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-1, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+
+      num.Initialize (".100111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-6, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+    }
+
+    [Test]
+    public void InitializeWithNegativeDecimalValue ()
+    {
+      BigNum num = new BigNum ();
+      num.Initialize ("-100.111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-3, num.Exponent);
+      Assert.AreEqual (true, num.Sign);
+
+      num.Initialize ("-10011.1");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-1, num.Exponent);
+      Assert.AreEqual (true, num.Sign);
+
+      num.Initialize ("-.100111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-6, num.Exponent);
+      Assert.AreEqual (true, num.Sign);
+
+    }
+
+    [Test]
+    public void InitializeWithLeadingAndTrailingZeroDecimalValues ()
+    {
+      BigNum num = new BigNum ();
+      num.Initialize ("0.100111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-6, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+
+      num.Initialize ("000.100111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-6, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+     
+      num.Initialize ("00022.1001");
+      Assert.IsTrue (num.Mantissa.Equals (221001));
+      Assert.AreEqual (-4, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+
+      num.Initialize ("0101.100");
+      Console.WriteLine ("mantissa: " + num.Mantissa.ToString ());
+      Assert.IsTrue (num.Mantissa.Equals (1011));
+      Assert.AreEqual (-1, num.Exponent);
+      Assert.AreEqual (false, num.Sign);
+
+      num.Initialize ("-0.100111");
+      Assert.IsTrue (num.Mantissa.Equals (100111));
+      Assert.AreEqual (-6, num.Exponent);
+      Assert.AreEqual (true, num.Sign);
+    }
+
+    [Test]
+    public void RemoveTralingZeros ()
+    {
+      BigNum num = new BigNum ();
+
+      string s = "1.10";
+      s = num.RemoveTrailingZeros (s);
+      Console.WriteLine ("s: " + s);
+      Assert.AreEqual ("1.1", s);
+
+      s = "1.100";
+      s = num.RemoveTrailingZeros (s);
+      Assert.AreEqual ("1.1", s);
+
+      s = "1.1010";
+      s = num.RemoveTrailingZeros (s);
+      Assert.AreEqual ("1.101", s);
+
+      s = "1.10100";
+      s = num.RemoveTrailingZeros (s);
+      Assert.AreEqual ("1.101", s);
+
+      s = "1.100100";
+      s = num.RemoveTrailingZeros (s);
+      Assert.AreEqual ("1.1001", s);
+    }
+
+    [Test]
+    public void RemoveTralingDecimalPoint ()
+    {
+      BigNum num = new BigNum ();
+
+      string s = "110.";
+      s = num.RemoveTrailingZeros (s);
+      Console.WriteLine ("s: " + s);
+      Assert.AreEqual ("110", s);
+    }
+
+
+    ////////////////////////////////////////////////////////////////
+    /// Tests For required methods
+    /// ////////////////////////////////////////////////////////////
+    ///
+
+    [Test]
+    public void ToStringTestWithIntegerValue ()
+    {
+      BigNum num = new BigNum ("1");
+      string expected = "1";
+      string actual = num.ToString ();
+    
+      Assert.IsFalse (actual.Contains (" "));
+      Assert.IsFalse (actual.Contains ("."));
+      Assert.AreEqual (expected, actual);
+    }
+
+    [Test]
+    public void ToStringTestWithNegativeIntegerValue ()
+    {
+      BigNum num = new BigNum ("-1");
+      string expected = "-1";
+      string actual = num.ToString ();
+
+      Assert.IsFalse (actual.Contains (" "));
+      Assert.IsFalse (actual.Contains ("."));
+      Assert.IsTrue (actual.Contains ("-"));
+      Assert.AreEqual (expected, actual);
+    }
+
+    [Test]
+    public void ToStringTestWithDecimalValue ()
+    {
+      BigNum num = new BigNum ("1.1");
+      string expected = "1.1";
+      string actual = num.ToString ();
+
+      Assert.IsFalse (actual.Contains (" "));
+      Assert.IsTrue (actual.Contains ("."));
+      Assert.AreEqual (expected, actual);
+    }
+
+    [Test]
+    public void ToStringTestWithNegativeDecimalValue ()
+    {
+      BigNum num = new BigNum ("-1.1");
+      string expected = "-1.1";
+      string actual = num.ToString ();
+
+      Assert.IsFalse (actual.Contains (" "));
+      Assert.IsTrue (actual.Contains ("."));
+      Assert.IsTrue (actual.Contains ("-"));
+      Assert.AreEqual (expected, actual);
+    }
+
+    [Test]
+    public void ToStringTestWithValueLessThanOne ()
+    {
+      BigNum num = new BigNum (".1");
+      string expected = ".1";
+      string actual = num.ToString ();
+
+      Assert.IsFalse (actual.Contains (" "));
+      Assert.IsFalse (actual.Contains ("0"));
+      Assert.IsTrue (actual.Contains ("."));
+      Console.WriteLine ("actual " + actual);
+      Assert.AreEqual (expected, actual);
+    }
+
+
+
+    
+    //    [Test]
+    //    public void ToStringTestWithRegularString ()
+    //    {
+    //      BigNum num = new BigNum ("1.1");
+    //      string expected = "";
+    //      string actual = num.ToString ();
+    //
+    //      Assert.IsFalse (actual.Contains (" "));
+    //      Assert.IsFalse (actual.Contains (" "));
+    //    }
+
+    //    [Test]
+    //    public void ToStringTestWithNoLeadingZeros ()
+    //    {
+    //      BigNum num = new BigNum ();
+    //      num.Initialize ("001.1");
+    //      string expected = "1.1";
+    //
+    //      string actual = num.ToString ();
+    //
+    //      Assert.AreEqual (expected, actual);
+    //    }
+
   }
 }
 
